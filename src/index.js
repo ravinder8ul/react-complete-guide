b9befd07df96c9c00154df2e62f60045df21acea
Ravinder Rakhara <ravinder.rakhara@gmail.com>
2021-01-26 19:08:49 -0500

    splitting an app into components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App appTitle="Person Manager"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
